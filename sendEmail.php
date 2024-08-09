a77328a59b2943d1533c1c8f563c5f76fb2cc6a5
Helen Hermosillo <helenh.4260@gmail.com>
2024-08-09 00:39:56 -0700

    Added a form that will handle sending emails

---
+++ sendEmail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "helenh.4260@gmail.com"; 
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+    $headers = "From: $email";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
+}
+?>
